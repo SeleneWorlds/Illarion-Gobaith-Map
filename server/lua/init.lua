4b180efd93aa8a5c67a3ec88c737d07aeb8e03ae
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-05-06 20:20:19 +0200

    refactor: Migrate to new script name conventions

--- server/lua/init.lua
+++ server/lua/init.lua
@@ -16,8 +16,8 @@ function convertMap(tilesFile)
     local unknownTiles = {}
     local unknownItems = {}
 
-    local map = Maps.Create()
-    local tilesInput = Resources.LoadAsString(tilesFile)
+    local map = Maps.create()
+    local tilesInput = Resources.loadAsString(tilesFile)
     local header = {}
     local startX, startY, z
     for line in tilesInput:gmatch("([^\n]*)\n?") do
@@ -45,34 +45,34 @@ function convertMap(tilesFile)
                     tileId = tileId & BASE_MASK
                 end
 
-                local tile = Registries.FindByMetadata("tiles", "tileId", tonumber(tileId))
+                local tile = Registries.findByMetadata("tiles", "tileId", tonumber(tileId))
                 if tile ~= nil and tileId ~= 0 then
-                    map:PlaceTile(startX + tonumber(x), startY + tonumber(y), z, tile.Name)
+                    map:placeTile(startX + tonumber(x), startY + tonumber(y), z, tile:getName())
                 elseif tileId ~= 0 then
                     unknownTiles[tileId] = unknownTiles[tileId] and unknownTiles[tileId] + 1 or 1
                 end
             end
         end
     end
-    local itemsInput = Resources.LoadAsString(basePath .. ".items.txt")
+    local itemsInput = Resources.loadAsString(basePath .. ".items.txt")
     for line in itemsInput:gmatch("([^\n]*)\n?") do
         if stringx.trim(line) ~= "" and stringx.trim(line):sub(1, 1) ~= "#" then
             -- Items are in the format X;Y;Item;Quality
             local x, y, itemId, quality = line:match("(-?%d+);(-?%d+);(-?%d+);(-?%d+)")
-            local tile = Registries.FindByMetadata("tiles", "itemId", tonumber(itemId))
+            local tile = Registries.findByMetadata("tiles", "itemId", tonumber(itemId))
             if tile == nil then
                 unknownItems[tonumber(itemId)] = quality
             else
-                map:PlaceTile(tonumber(x) + startX, tonumber(y) + startY, z, tile.Name)
+                map:placeTile(tonumber(x) + startX, tonumber(y) + startY, z, tile:getName())
             end
         end
     end
-    local warpsInput = Resources.LoadAsString(basePath .. ".warps.txt")
+    local warpsInput = Resources.loadAsString(basePath .. ".warps.txt")
     for line in warpsInput:gmatch("([^\n]*)\n?") do
         if stringx.trim(line) ~= "" and stringx.trim(line):sub(1, 1) ~= "#" then
             -- Warps are in the format X;Y;ToX;ToY;ToLevel
             local x, y, toX, toY, toLevel = line:match("(-?%d+);(-?%d+);(-?%d+);(-?%d+);(-?%d+)")
-            map:AnnotateTile(tonumber(x) + startX, tonumber(y) + startY, z, "illarion:warp", {
+            map:annotateTile(tonumber(x) + startX, tonumber(y) + startY, z, "illarion:warp", {
                 x = tonumber(x) + startX,
                 y = tonumber(y) + startY,
                 z = tonumber(toLevel) - 31000
@@ -94,7 +94,7 @@ end
 
 local mergedMapPath = "maps/illarion-gobaith.selenemap"
 if not Saves.Has(mergedMapPath) then
-    local mergedMap = Maps.Create()
+    local mergedMap = Maps.create()
     local files = Resources.ListFiles("illarion-gobaith-map", "server/maps/*.tiles.txt")
     for _, file in pairs(files) do
         local outputPath = convertMap(file)
@@ -110,4 +110,4 @@ end
 
 print("Loading " .. mergedMapPath)
 local gobaithMap = Saves.Load(mergedMapPath)
-Dimensions.GetDefault().Map = gobaithMap
+Dimensions.getDefault():setMap(gobaithMap)
